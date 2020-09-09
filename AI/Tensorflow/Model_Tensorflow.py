b1afebc42d2724012b931c8bdb28e5d5ec42ad31
Sergigd <sergi.gd.98@gmail.com>
2020-09-09 10:32:04 +0200

    Commit: Tensorflow first model with keras.

--- AI/Tensorflow/Model_Tensorflow.py
+++ AI/Tensorflow/Model_Tensorflow.py
@@ -4,65 +4,42 @@ from tensorflow.keras.layers import Dense
 import pandas as pd
 import DataBase.DataSource
 
-# 1) import data
+
 # Load questions from DB
 data = DataBase.DataSource.data()
 questions_and_id_db = data.get_questions_and_id_dB()
-
 questions_db = []
 y = []
 for question in questions_and_id_db:
     questions_db.append(question[0])
     y.append(question[1])
 
-# print(questions_db)
-# print(y)
-
-iris = datasets.load_iris()
-# print("iris:")
-# print(iris)
-#
-# # 2) prepare inputs
-# input_x = iris.data
-#
-# # 3) prepare outputs: a binary class matrix
-#
-# # output_y = keras.utils.to_categorical(iris.target, 3)
+# Counting
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(strip_accents='ascii', lowercase=True, stop_words='english')
 X = vectorizer.fit_transform(questions_db)
-# import pickle
-# filename = "Vectorizer/CountVectorizer.pkl"
-# pickle.dump(vectorizer, open(filename, 'wb'))
 
-results = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())
-# results['Question_id'] = y  # Concatenate X and y
+# Saving CountVectorizer
+import pickle
+filename = "Vectorizer/CountVectorizer.pkl"
+pickle.dump(vectorizer, open(filename, 'wb'))
 
-# print("data")
-# print(iris.data)
-# print("Vectorizer")
-# print(vectorizer.get_feature_names())
-# print("Results")
-# print(results)
-# results_id = results.pop("Question_id")
-results_id = y
+# Prepare outputs: a binary class matrix
+output_y = keras.utils.to_categorical(y)  # (results_id, 30)?
 
-output_y = keras.utils.to_categorical(results_id)  # (results_id, 30)?
-
-# 4a) Create the model
+# Create the model
 model = keras.models.Sequential()
 model.add(Dense(8, input_dim=95, activation='relu'))
 model.add(Dense(31, activation='softmax'))
 
-# 4b) Compile model
+# Compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 4c) Fit the model
+# Fit the model
 model.fit(X, output_y, epochs=150, batch_size=15, verbose=1)
 
-# 4d) Evaluate the model
+# Evaluate the model
 score = model.evaluate(X, output_y, batch_size=15)
-# score
 
 # Test
 text = vectorizer.transform(["how to merge"])
@@ -80,3 +57,11 @@ print("Index = ", index)
 print("Text = ", text)
 title = data.get_title_by_id(index)
 print("Result = ", title)
+
+
+# Saving model
+yes_no = input('Save?:')
+if yes_no == 'y':
+    # Save to file in the current working directory
+    model.save("Models/TFKeras_Count_v1.h5")
+
---
+++ AI/Tensorflow/TFKeras_Model_Test.py
@@ -0,0 +1,47 @@
+import pickle
+import os
+from AI.Distance import Distance_Methods as nlp
+from tensorflow import keras
+
+
+# Files:
+# Path
+root_dir = os.path.dirname(os.path.abspath(os.curdir))
+path = os.path.join(root_dir, "Tensorflow")
+# Count
+file_model = os.path.join(path, "Models", "TFKeras_Count_v1.h5")
+file_vector = os.path.join(path, "Vectorizer", "CountVectorizer.pkl")
+# # TF-IDF
+# file_model = os.path.join(path, "Models", "DT_Tfidf_v1.pkl")
+# file_vector = os.path.join(path, "Vectorizer", "TFIDFVectorizer.pkl")
+
+
+# Loading Test Questions from Test_Similar_Questions.txt
+questions_similar = nlp.read_questions_similar()
+
+# Loading Keras
+loaded_model = keras.models.load_model(file_model)
+# loaded_model = pickle.load(open(file_model, 'rb'))
+
+# Loading CountVectorizer
+vectorizer = pickle.load(open(file_vector, 'rb'))
+
+# Test with questions
+good = 0
+attempts = 0
+for question in questions_similar:
+    question_x = vectorizer.transform([question])
+    predict_model = loaded_model.predict(question_x)
+    max_predict = max(predict_model[0])
+    for index, value in enumerate(predict_model[0]):
+        if value == max_predict:
+            break
+
+    title = questions_similar[index - 1]
+    attempts += 1
+    if question == title:
+        good += 1
+    # print("Question: ", question)
+    # print("Answer: ", title)
+
+print("The model: {0} \nGood: {1}\nAttempts: {2}".format(file_model, good, attempts))
